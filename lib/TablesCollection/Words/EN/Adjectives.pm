17ad03d70474449de7a46e45427d696aa4008c77
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-05 08:51:24 +0700

    Initial commit

---
+++ lib/TablesCollection/Words/EN/Adjectives.pm
@@ -0,0 +1,22 @@
+package TablesCollection::Words::EN::Adjectives;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of Tables:: modules that contain English adjectives
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<Tables>:: modules:
+
+# INSERT_MODULES_LIST /^Tables::/
+
+
+=head1 SEE ALSO
+
+L<Tables>
+
+=cut
